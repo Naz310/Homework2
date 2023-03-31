168fa6e87edb91058fb3d396edd77d186136092c
Nazira Jamankulova <summert2024@gmail.com>
2023-03-31 20:27:50 +0600

    my homeWork2

---
+++ homework2.dart
@@ -0,0 +1,63 @@
+void main() {
+  int a = 300;
+  int b = 200;
+  int c = 100;
+  int d = 50;
+  int e = 400;
+  print(a + b + c + d + e);
+
+  double a1 = 2.1;
+  double b2 = 5.3;
+  double c3 = 7.6;
+  double d4 = 5.4;
+  double e5 = 7.8;
+  print(a1 + b2 + c3 + d4 + e5);
+
+  String msg1 = "I live in Bishkek";
+  String msg2 = "I would like a coffee";
+  String msg3 = "I am a full time employee";
+  String msg4 = "The weather is wonderful!";
+  String msg5 = "Have a nice day";
+  print(msg3);
+
+  List array1 = [1, 2, 3];
+  List array2 = [a, b, c, d];
+  List array3 = ['ya', 'ty', "oni"];
+  List fruits = ["apple", "grape", "banana"];
+  List names = ["Nazira", "Asel", "Zarina"];
+  print(array3);
+
+  Map nazira = {'name': 'Nazira', 'location': 'Bishkek'};
+  Map product = {'product': 'coffee', 'price': 300};
+  Map fugires = {'first': 1, 'second': 2};
+  Map prices = {'car': 10000, "house": 150000};
+  Map bank = {'name': 'DKIB', 'branch': 'main', 'adress': 'Chuy Str.,245'};
+  print(product);
+
+  String smileSymbole = "\u{1F600}";
+  String symbol2 = '\u{1f604}';
+  String symbol3 = '\u0063';
+  String symbol4 = '\u0064';
+  print(symbol2);
+
+  final v = 123;
+  final v1 = 456;
+  final v2 = 'abc';
+  final v3 = 789;
+  final v4 = '789';
+  print(v4);
+
+  dynamic y = 123;
+  dynamic y2 = 456;
+  dynamic y3 = 'abc';
+  dynamic y4 = 789;
+  dynamic y5 = 'def';
+  print(y5);
+
+  var vv = 123;
+  var vv1 = 456;
+  var vv2 = 789;
+  var vv3 = 101112;
+  var vv4 = "jh";
+  print(y4);
+}
